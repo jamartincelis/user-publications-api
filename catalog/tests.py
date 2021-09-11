46cfb5576ff03f9c67741896f4dde4ef33972689
Javier <jmartin@darient.com>
2021-09-11 05:31:03 -0400

    Se agregan test modulo catalog

--- catalog/tests.py
+++ catalog/tests.py
@@ -18,15 +18,32 @@ class CatalogsTestCase(TestCase):
 
     def test_list_all_codes(self):
         response = self.client.get('/catalog/codes_list/')
-        # print(response.json())
         self.assertEqual(Code.objects.all().count(), len(response.json()))
 
     def test_list_all_catalogs(self):
         response = self.client.get('/catalog/list/')
         self.assertEqual(CodeType.objects.all().count(), len(response.json()))
 
-    def test_filter_catalogs_by_name(self):
-        response = self.client.get('/catalog/project_categories/')
+    def test_filter_catalogs_by_name_account_status(self):
+        response = self.client.get('/catalog/account_status/')
         data = response.json()
-        # print(json.dumps(data, indent=2, ensure_ascii=False))
-        self.assertEqual(response.json()['name'], 'project_categories')
+        code_type = CodeType.objects.get(name="account_status")
+        self.assertEqual(data['name'], code_type.name)
+        self.assertEqual(data['id'], str(code_type.id))
+        self.assertEqual(data['description'], str(code_type.description))
+        
+    def test_filter_catalogs_by_name_transaction_category(self):
+        response = self.client.get('/catalog/transaction_category/')
+        data = response.json()
+        code_type = CodeType.objects.get(name="transaction_category")
+        self.assertEqual(data['name'], code_type.name)
+        self.assertEqual(data['id'], str(code_type.id))
+        self.assertEqual(data['description'], str(code_type.description))
+
+    def test_filter_catalogs_by_name_account_type(self):
+        response = self.client.get('/catalog/account_type/')
+        data = response.json()
+        code_type = CodeType.objects.get(name="account_type")
+        self.assertEqual(data['name'], code_type.name)
+        self.assertEqual(data['id'], str(code_type.id))
+        self.assertEqual(data['description'], str(code_type.description))
