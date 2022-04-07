b4141e8be4e185ed361024f325c0beb2cb5153d8
Javier <javier@ubankapp.com>
2022-04-07 17:41:11 -0400

    FIX: settings.py

--- src/pfmservice/settings.py
+++ src/pfmservice/settings.py
@@ -90,12 +90,27 @@ WSGI_APPLICATION = 'pfmservice.wsgi.application'
 
 # se desactiva el uso de zona horaria
 # para evitar errores en los querys
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': environ.get('DATABASE_NAME')
-    }
-}
+if 'test' in sys.argv or 'test_coverage' in sys.argv: #Covers regular testing and django-coverage
+    USE_TZ = False
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3'
+        }
+    }    
+else:
+    USE_TZ = True
+    TIME_ZONE = environ.get('TIME_ZONE')
+    
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': environ.get('DATABASE_NAME'),
+            'USER': environ.get('DATABASE_USER'),
+            'PASSWORD': environ.get('DATABASE_PASSWORD'),
+            'HOST': environ.get('DATABASE_HOST'),
+            'PORT': environ.get('DATABASE_PORT'),
+        }
+    } 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
